05009c74268dc731678f88eb50031d7a46437cc4
dashu19 <46594899+dashu19@users.noreply.github.com>
2019-04-09 15:14:28 -0400

    Update py.py

--- py.py
+++ py.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 print("Hello World!")
 
@@ -6,27 +7,51 @@ S = requests.Session()
 
 URL = "https://en.wikipedia.org/w/api.php"
 
-Params = {
-    "action": "query",
-    "format": "json",
-    "titles": "Albert Einstein",
-    "prop": "links",
+##Params = {
+##    "action": "query",
+##    "format": "json",
+##    "titles": "Albert Einstein",
+##    "prop": "links",
+##    }
+##
+##R = S.get(URL, params = Params)
+##Data = R.json()
+##
+##Key = list(Data["query"]['pages'].keys())
+##
+##print(Key)
+##
+##Data = Data["query"]['pages'][Key[0]]['links']
+##
+##titles = []
+##
+##for x in Data:
+##    print(x['title'])
+##    titles += [x['title']]
+##
+##print(titles)
+
+def getlinkstitles(title):
+    rparams = {
+        "action": "query",
+        "format": "json",
+        "titles": title,
+        "prop": "links",
     }
 
-R = S.get(URL, params = Params)
-Data = R.json()
+    R = S.get(URL, params = rparams)
+    Data = R.json()
+    mykey = list(Data["query"]['pages'].keys())
 
-Key = list(Data["query"]['pages'].keys())
+    Data = Data["query"]['pages'][mykey[0]]['links']
+    titles = []
 
-print(Key)
+    for x in Data:
+        titles += [x['title']]
 
-Data = Data["query"]['pages'][Key[0]]['links']
+    return titles
 
-titles = []
+print(getlinkstitles('Albert Einstein'))
 
-for x in Data:
-    print(x['title'])
-    titles += [x['title']]
-
-print(titles)
+    
 
